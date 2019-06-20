8f3c8592c82a931ebc081eff35135b2ac50688d3
69ghostrider <joshuasalema@gmail.com>
2019-06-20 18:53:09 +0530

    Comitting code for Recipe

--- Grocery-App/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Grocery-App/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[]  = [
+    new Recipe('Test Recipe', 'This is simple a test', 'https://tinyurl.com/y3vysn2h'),
     new Recipe('Test Recipe', 'This is simple a test', 'https://tinyurl.com/y3vysn2h')
   ];
   constructor() { }
