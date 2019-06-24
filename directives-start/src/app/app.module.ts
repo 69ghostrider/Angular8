10be63b579ee24eba264ed785865b67f81edb4e3
69ghostrider <joshuasalema@gmail.com>
2019-06-24 17:54:23 +0530

    Comitting code for dropdown

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighlightDirective
+    BasicHighlightDirective,
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
