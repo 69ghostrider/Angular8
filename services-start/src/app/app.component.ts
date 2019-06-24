21cdfe1e851b3d6517f9be5998c49d1ea803d81e
69ghostrider <joshuasalema@gmail.com>
2019-06-24 19:34:39 +0530

    Adding account service and logger service

---
+++ services-start/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { AccountsService } from './accounts.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers:[AccountsService]
+})
+export class AppComponent implements OnInit {
+  
+  accounts : {name: string, status:string}[] = [];
+
+  constructor(private accountsService: AccountsService){}
+
+  ngOnInit(){
+    this.accounts=this.accountsService.accounts;
+  }
+  // onAccountAdded(newAccount: {name: string, status: string}) {
+  //   this.accountsService.accounts.push(newAccount);
+  // }
+
+  // onStatusChanged(updateInfo: {id: number, newStatus: string}) {
+  //   this.accountsService.accounts[updateInfo.id].status = updateInfo.newStatus;
+  // }
+}
