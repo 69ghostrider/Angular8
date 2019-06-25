0b418e129a3910b056aaf4a03b649c368b4dea57
69ghostrider <joshuasalema@gmail.com>
2019-06-25 10:56:38 +0530

    Comitting code for Account Service

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,3 +1,4 @@
+import { AccountsService } from './../accounts.service';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { LoggingService } from './../logging.service';
 
@@ -5,18 +6,12 @@ import { LoggingService } from './../logging.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers:[LoggingService]    //injecting a service
+  providers:[LoggingService]    //injecting a service  //no adding AccountsService as it it declared in parent and will be overitten
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
-  constructor(private loggingService: LoggingService){}
+  constructor(private loggingService: LoggingService, private accountService: AccountsService){}
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    //console.log('A server status changed, new status: ' + accountStatus);
-   // const service = new LoggingService();
+    this.accountService.addAccount(accountName,accountStatus);
     this.loggingService.logStatusChange(accountStatus)   //creating a logger
   }
 }
